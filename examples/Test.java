6a152a4793cdac8ac6ee275a01808f2096df6bd3
Hannes Mehnert <hannes@mehnert.org>
2012-07-27 14:45:49 +0200

    add example

---
+++ examples/Test.java
@@ -0,0 +1,10 @@
+
+public class Test {
+  <% 20 euro %>
+  private static int foo() {
+    return 10; <% 10 euro %>
+  }
+  public static void main(String[] args) {
+    System.out.println(foo());
+  }
+}
